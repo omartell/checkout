2eca85dac71831bf262b457f4e7d5743b411b5fe
Oliver Martell <oliver.martell@gmail.com>
2013-05-23 17:46:04 -0500

    Better names

--- lib/checkout/checkout.rb
+++ lib/checkout/checkout.rb
@@ -1,13 +1,13 @@
 module Checkout
   class Checkout
-    def initialize(rules)
-      @rules = rules
+    def initialize(pricing_rules)
+      @pricing_rules    = pricing_rules
       @products_scanned = []
     end
 
     def total
-      @products_scanned.inject(0.0) do |memo, item|
-        memo + @rules.fetch(item).fetch(:unit_price)
+      @products_scanned.inject(0.0) do |total, item|
+        total + @pricing_rules.fetch(item).fetch(:unit_price)
       end
     end
 
